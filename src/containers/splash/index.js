824134095ea52bf27056c00722e5cfbfb8d53e66
esraa ragab <esraa.mo.ragab@gmail.com>
2019-04-18 02:41:04 +0200

    Add Splash Screen

---
+++ src/containers/splash/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import { Image, ImageBackground, StyleSheet, Text } from 'react-native'
+import { AsyncStorage } from 'react-native';
+import {TEXTS,COLORS} from '../../common';
+import firebase from '../../firebase';
+
+class SplashScene extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged((user) => {
+      setTimeout( () => {
+        if(user) {
+          this.props.navigation.navigate('HomeScreen');
+        } else {
+          this.props.navigation.navigate('LoginScreen');
+        }
+      },1000);
+    });
+  }
+
+  render() {
+      return (
+        <ImageBackground
+          source={require('../../images/cover.png')}
+          style={{width: '100%', height: '100%'}}
+        >
+        <Image resizeMode={'contain'} style={{width: 100, height: "100%", alignSelf: 'center'}}
+          source={require('../../images/logo.png')}/>
+        </ImageBackground>
+      );
+  }
+}
+
+export default SplashScene;
+
+var styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#250454',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    marginBottom: 20,
+  },
+  logoText: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  imageStyle: {
+    height: 100,
+    width: 100,
+  },
+})
