16323f775ca145df0f538ad2e412161affe690f3
esraa ragab <esraa.mo.ragab@gmail.com>
2019-04-18 17:27:04 +0200

    add header file

---
+++ src/components/Header/index.js
@@ -0,0 +1,95 @@
+import React,{Component} from 'react'
+import { StyleSheet, Text, View, Image, Platform, TouchableOpacity } from 'react-native'
+import { NavigationActions } from 'react-navigation'
+import { COLORS, EX } from '../../common';
+import Feather from 'react-native-vector-icons/Feather';
+
+class Header extends Component {
+
+  openDrawer = () => {
+    navigation.openDrawer()
+  }
+
+  render() {
+    const {
+        navigation,
+        title,
+        date,
+        search,
+        menu,
+        back,
+        underline
+    } = this.props;
+
+    return (
+      <View style={[styles.container,{borderBottomWidth: underline == 1 ? 0 : 5,}]}>
+      { menu == false ? null : <TouchableOpacity style={[styles.socialIconView,{ marginLeft: 5,marginRight: 5, justifyContent: 'center', alignItems: 'center',}]} onPress = {() => {
+          navigation.openDrawer()
+        }
+      }>
+          <Feather style={styles.socialIcons} name={'menu'}/>
+      </TouchableOpacity>
+
+      }
+        { search == false ? null : <TouchableOpacity style={[styles.socialIconView]}>
+                <Feather style={styles.socialIcons} name={'search'}/>
+            </TouchableOpacity> }
+        { back ? <TouchableOpacity style={[styles.socialIconView]} onPress={()=>this.props.navigation.goBack()}>
+                <Feather style={styles.socialIcons} name={'arrow-left'}/>
+            </TouchableOpacity> : null }
+        <View style={{flex: 1, marginLeft: 5,marginRight: 5, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+
+
+      </View>
+    );
+  }
+}
+
+export {Header};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: COLORS.main,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 5,
+    width: EX.width,
+    borderColor: COLORS.gold,
+    height: Platform.OS === 'ios' ? 50 : 45,
+  },
+  uglyDrawerItem: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#891B05',
+    padding: 15,
+    margin: 5,
+    borderRadius: 2,
+    borderColor: '#891B05',
+    borderWidth: 1,
+    textAlign: 'center'
+  },
+  socialIconView: {
+    height: 30,
+    width: 30,
+  },
+  socialIcons: {
+    color: COLORS.white,
+    fontSize: 30,
+    alignSelf: 'center'
+  },
+  title: {
+    fontSize: 20,
+    color: COLORS.white,
+    textAlign: 'center',
+    alignSelf: 'center'
+  },
+  date: {
+    fontSize: 12,
+    color: COLORS.white,
+    textAlign: 'center'
+  }
+})
