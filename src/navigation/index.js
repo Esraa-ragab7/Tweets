6ea944be1a3233976615516458548d670f81f12c
esraa ragab <esraa.mo.ragab@gmail.com>
2019-04-18 17:29:26 +0200

    Navigation construct.

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,12 +1,24 @@
 //Navigation File
-import {createStackNavigator, createAppContainer} from 'react-navigation';
+import {createStackNavigator, createAppContainer, createDrawerNavigator, createSwitchNavigator} from 'react-navigation';
 import {COLORS} from '../common';
 import Login from '../containers/login';
 import Home from '../containers/home';
+import SignUp from '../containers/signUp';
+import Splash from '../containers/splash';
+import Drawer from '../containers/home/drawer';
+import Profile from '../containers/profile';
+
+const DrawerNavigation = createDrawerNavigator({
+  MainPageScreen : Home,
+  ProfileScreen: Profile,
+}, {
+  contentComponent: Drawer,
+  gesturesEnabled: false
+})
 
 const NavStack = createStackNavigator({
     LoginScreen : Login,
-    HomeScreen : Home
+    SignUpScreen : SignUp
 },{
     initialRouteName : 'LoginScreen',
     navigationOptions : ({navigation})=>{
@@ -19,6 +31,16 @@ const NavStack = createStackNavigator({
     }
 })
 
-const AppNavigation = createAppContainer(NavStack);
+
+const RootNavigation = createSwitchNavigator({
+    SplashScreen : Splash,
+    LoginScreen : NavStack,
+    HomeScreen : DrawerNavigation,
+},{
+    initialRouteName : 'SplashScreen'
+});
+
+
+const AppNavigation = createAppContainer(RootNavigation);
 
 export default AppNavigation;
